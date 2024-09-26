a28f8919a8a61aea7ac91e83c48f862b99bb2c7a
kimsohea <kimsohea4425@gmail.com>
2024-09-26 11:06:09 +0900

    remove: 사용하지 않는 api 구문 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    proxy: {
-      aladin: {
-        target: "https://www.aladin.co.kr/ttb/api/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/aladin/, ""),
-      },
-    },
   },
 });
